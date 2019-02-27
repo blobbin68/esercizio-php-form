267d82be862d81a0462699c6b88c4e0b7c4fa77b
Cesare-Sari <cesaresari2003@gmail.com>
2019-02-27 10:14:53 +0100

    aggiunto form

---
+++ index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <title>Forms</title>
+    </head>
+      <body>
+        <div class="container">
+           <form>
+            <div class="form-group">
+              <label for="name">inserisci il tuo nome "o nomignolo"</label>
+               <input type="text" class="form-control" id="name" aria-describedby="nameHelp" placeholder="Il Tuo nome :)">
+                <small id="nameHelp" class="form-text text-muted"> inserisci il tuo nome cosi veniamo a derubarti</small>
+                  </div>
+                   <div class="container">
+                    
+                      <div class="form-group">
+                      <label for="surname">inserisci il tuo cognome "o quello che vuoi >:C"</label>
+                      <input type="text" class="form-control" id="surname" aria-describedby="surnameHelp" placeholder="Il Tuo cognome :)">
+                     <small id="surnameHelp" class="form-text text-muted">inserisci il cognome cosi abbiamo piu dati per trovarti</small>
+                   <button type="submit" class="btn btn-primary">invia i tuoi dati >:)</button>
+                  </div>
+                 </form>
+               </div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="js/jquery-3.3.1.min"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="bootstrap.min.js"></script>
+  </body>
+</html>
